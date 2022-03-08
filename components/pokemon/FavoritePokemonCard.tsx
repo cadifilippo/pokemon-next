b575fc66f172e55a1ecfb6b230b5db0d9ef4bc94
Carlos Difilippo <cadifilippo@gmail.com>
2022-03-07 21:46:46 -0300

    add navegation from favorites to pokemon page

--- components/pokemon/FavoritePokemonCard.tsx
+++ components/pokemon/FavoritePokemonCard.tsx
@@ -1,10 +1,17 @@
 import { Grid, Card } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 
 export const FavoritePokemonCard: FC<{ id: number }> = ({ id }) => {
+  const router = useRouter();
+
+  const onFavoriteClick = () => {
+    router.push(`/pokemon/${id}`);
+  };
+
   return (
     <Grid xs={6} sm={3} xl={1} key={id}>
-      <Card hoverable clickable css={{ padding: 10 }}>
+      <Card hoverable clickable css={{ padding: 10 }} onClick={onFavoriteClick}>
         <Card.Image
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
           height="100%"
