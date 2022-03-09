22ad095adafca8d02d7396ae00d82caa8ab83e93
Carlos Difilippo <cadifilippo@gmail.com>
2022-03-08 21:57:52 -0300

    add Open Graph Meta Tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,6 +6,8 @@ interface Props {
   title?: string;
 }
 
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
@@ -14,6 +16,13 @@ export const Layout: FC<Props> = ({ children, title }) => {
         <meta name="author" content="Carlos Difilippo" />
         <meta name="description" content="Pokemon App" />
         <meta name="keywords" content="pokemon, app, react, next" />
+
+        <meta property="og:title" content="Información sobre los pokemones" />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}.`}
+        />
+        <meta property="og:image" content={`${origin}/images/banner.png`} />
       </Head>
 
       <Navbar />
